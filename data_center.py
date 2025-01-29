5f07d1682664f5559558aeebd1caea12e3d4beb7
lolo-ran <lauren.hunter@berkeley.edu>
2025-01-29 02:06:50 +0000

    changed print to st.write

--- data_center.py
+++ data_center.py
@@ -11,26 +11,12 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
-# Create an empty list to hold packet data
-packet_data = []
-
-# Function to decode packet
-def decode_packet(data):
-    # Define the structure of the packet: 6 int16_t values + 4 int values
-    # 'h' for int16_t (2 bytes) and 'i' for int (4 bytes)
-    format_string = '6h4i'  # 6 int16_t (h) and 4 int (i)
-    
-    # Unpack the data based on the format
-    decoded_values = struct.unpack(format_string, data)
-    
-    return decoded_values
-
 while True:
     # Receive data (choose a buffer size that matches the data you're receiving)
     data, addr = sock.recvfrom(28)  
     
     if len(data) == 28:
-        print(f"Received packet from {addr}")
+        st.write(f"Received packet from {addr}")
         
         # Unpack the data (we know the structure: 6 int16_t and 4 int)
         # First unpack the 2-byte int16_t values (6 values)
@@ -39,9 +25,9 @@ while True:
         read_1_0, read_1_1, read_2_0, read_2_1 = struct.unpack('<4i', data[12:])
         
         # Print the unpacked data (you can process it as needed)
-        print(f"x_accel: {x_accel}, y_accel: {y_accel}, z_accel: {z_accel}")
-        print(f"x_gyro: {x_gyro}, y_gyro: {y_gyro}, z_gyro: {z_gyro}")
-        print(f"read_1_0: {read_1_0}, read_1_1: {read_1_1}")
-        print(f"read_2_0: {read_2_0}, read_2_1: {read_2_1}")
+        st.write(f"x_accel: {x_accel}, y_accel: {y_accel}, z_accel: {z_accel}")
+        st.write(f"x_gyro: {x_gyro}, y_gyro: {y_gyro}, z_gyro: {z_gyro}")
+        st.write(f"read_1_0: {read_1_0}, read_1_1: {read_1_1}")
+        st.write(f"read_2_0: {read_2_0}, read_2_1: {read_2_1}")
     else:
-        print("Received packet of incorrect size")
+        st.write("Received packet of incorrect size")
