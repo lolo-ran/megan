ddc36c5484f44c072b164d78fce4b1af66a6614e
lolo-ran <lauren.hunter@berkeley.edu>
2025-01-30 12:46:59 -0800

    Updated size of packet to 94 bytes

--- data_center.py
+++ data_center.py
@@ -15,14 +15,14 @@ while True:
     # Receive data (choose a buffer size that matches the data you're receiving)
     data, addr = sock.recvfrom(28)  
     
-    if len(data) == 28:
+    if len(data) == 94:
         st.write(f"Received packet from {addr}")
         
-        # Unpack the data (we know the structure: 6 int16_t and 4 int)
+        # Unpack the data (we know the structure: 6 int16_t and 24 int)
         # First unpack the 2-byte int16_t values (6 values)
         x_accel, y_accel, z_accel, x_gyro, y_gyro, z_gyro = struct.unpack('<6h', data[:12])
-        # Then unpack the 4-byte int values (4 values)
-        read_1_0, read_1_1, read_2_0, read_2_1 = struct.unpack('<4i', data[12:])
+        # Then unpack the 4-byte int values (24 values)
+        read_1_0, read_1_1, read_1_2, read_1_3, read_1_4, read_1_5, read_1_6, read_1_7, read_2_0, read_2_2, read_2_3, read_2_4, read_2_5, read_2_6, read_2_7, read_3_0, read_3_1, read_3_2, read_3_3, read_3_4, read_3_5, read_3_6, read_3_7 = struct.unpack('<24i', data[12:])
         
         # Print the unpacked data (you can process it as needed)
         st.write(f"x_accel: {x_accel}, y_accel: {y_accel}, z_accel: {z_accel}")
