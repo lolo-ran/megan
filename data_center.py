dfa2cc35ae77214618a29a0ee079b1bfd80f2be0
lolo-ran <lauren.hunter@berkeley.edu>
2025-01-30 21:06:17 +0000

    Writing all bytes to the site

--- data_center.py
+++ data_center.py
@@ -22,12 +22,13 @@ while True:
         # First unpack the 2-byte int16_t values (6 values)
         x_accel, y_accel, z_accel, x_gyro, y_gyro, z_gyro = struct.unpack('<6h', data[:12])
         # Then unpack the 4-byte int values (24 values)
-        read_1_0, read_1_1, read_1_2, read_1_3, read_1_4, read_1_5, read_1_6, read_1_7, read_2_0, read_2_2, read_2_3, read_2_4, read_2_5, read_2_6, read_2_7, read_3_0, read_3_1, read_3_2, read_3_3, read_3_4, read_3_5, read_3_6, read_3_7 = struct.unpack('<24i', data[12:])
+        read_1_0, read_1_1, read_1_2, read_1_3, read_1_4, read_1_5, read_1_6, read_1_7, read_2_0, read_2_1, read_2_2, read_2_3, read_2_4, read_2_5, read_2_6, read_2_7, read_3_0, read_3_1, read_3_2, read_3_3, read_3_4, read_3_5, read_3_6, read_3_7 = struct.unpack('<24i', data[12:])
         
         # Print the unpacked data (you can process it as needed)
         st.write(f"x_accel: {x_accel}, y_accel: {y_accel}, z_accel: {z_accel}")
         st.write(f"x_gyro: {x_gyro}, y_gyro: {y_gyro}, z_gyro: {z_gyro}")
-        st.write(f"read_1_0: {read_1_0}, read_1_1: {read_1_1}")
-        st.write(f"read_2_0: {read_2_0}, read_2_1: {read_2_1}")
+        st.write(f"read_1_0: {read_1_0}, read_1_1: {read_1_1}, read_1_2: {read_1_2}, read_1_3: {read_1_3}, read_1_4: {read_1_4}, read_1_5: {read_1_5}, read_1_6: {read_1_6}, read_1_7: {read_1_7}")
+        st.write(f"read_2_0: {read_2_0}, read_2_1: {read_2_1}, read_2_2: {read_2_2}, read_2_3: {read_2_3}, read_2_4: {read_2_4}, read_2_5: {read_2_5}, read_2_6: {read_2_6}, read_2_7: {read_2_7}")
+        st.write(f"read_3_0: {read_3_0}, read_3_1: {read_3_1}, read_3_2: {read_3_2}, read_3_3: {read_3_3}, read_3_4: {read_3_4}, read_3_5: {read_3_5}, read_3_6: {read_3_6}, read_3_7: {read_3_7}")
     else:
         st.write("Received packet of incorrect size")
